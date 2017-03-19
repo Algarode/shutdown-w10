29e625bb8281836648bf785a1f9118ac17c06403
Edwin Lambregts <edwin.lambregts0@gmail.com>
2017-03-19 13:52:09 +0100

    Roundup

---
+++ SDComputer/SDComputer/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SDComputer
+{
+    partial class Shutdown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rb30 = new System.Windows.Forms.RadioButton();
+            this.rb60 = new System.Windows.Forms.RadioButton();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rb30
+            // 
+            this.rb30.AutoSize = true;
+            this.rb30.Location = new System.Drawing.Point(13, 13);
+            this.rb30.Name = "rb30";
+            this.rb30.Size = new System.Drawing.Size(56, 17);
+            this.rb30.TabIndex = 0;
+            this.rb30.TabStop = true;
+            this.rb30.Text = "30 min";
+            this.rb30.UseVisualStyleBackColor = true;
+            // 
+            // rb60
+            // 
+            this.rb60.AutoSize = true;
+            this.rb60.Location = new System.Drawing.Point(173, 13);
+            this.rb60.Name = "rb60";
+            this.rb60.Size = new System.Drawing.Size(56, 17);
+            this.rb60.TabIndex = 1;
+            this.rb60.TabStop = true;
+            this.rb60.Text = "60 min";
+            this.rb60.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(13, 53);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(216, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(13, 83);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(216, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Shutdown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 118);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.rb60);
+            this.Controls.Add(this.rb30);
+            this.Name = "Shutdown";
+            this.Text = "Shutdown";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rb30;
+        private System.Windows.Forms.RadioButton rb60;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ SDComputer/SDComputer/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SDComputer
+{
+    public partial class Shutdown : Form
+    {
+        public Shutdown()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            if (rb30.Checked)
+            {
+                i = 1800; // 30 minutes
+            }
+            else if (rb60.Checked)
+            {
+                i = 3600; // 60 minutes
+            }
+
+            Process.Start("shutdown", "/s /t " + i);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Process.Start("shutdown", "-a");
+        }
+    }
+}
